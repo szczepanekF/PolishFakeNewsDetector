31041f3556fc182df2cfd608f3b24bdf89c36221
Filip Szczepanek <107700989+szczepanekF@users.noreply.github.com>
2025-04-26 17:29:58 +0200

    Auth business logic,, fixes, and logstash integration
    Co-authored-by: Jakub <j.kowalik0928@gmail.com>

---
+++ BusinessLogicService/src/main/java/com/pfnd/BusinessLogicService/BusinessLogicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pfnd.BusinessLogicService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessLogicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessLogicServiceApplication.class, args);
+	}
+
+}
