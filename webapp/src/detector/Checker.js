f2b1bfd1b74207e9f4fe46677b4855bb55b0cc79
AMalinowska03 <242463@edu.p.lodz.pl>
2025-06-16 03:18:29 +0200

    Google should be connected, evaluation checker to check with working ml

--- webapp/src/detector/Checker.js
+++ webapp/src/detector/Checker.js
@@ -7,8 +7,10 @@ import {toast} from "react-toastify";
 
 export default function Checker({userToken, setGuest}){
     const [checkForm, setCheckForm] = useState({text: ""});
-    const [answer, setAnswer] = useState("");
-    const [status, setStatus] = useState(0);
+    const [progress, setProgress] = useState({currentStep: 0, allSteps: 0});
+    const [isLoading, setIsLoading] = useState(false);
+    const [result, setResult] = useState(null);
+    const [evaluationId, setEvaluationId] = useState(null);
     const [textEmpty, setTextEmpty] = useState(false);
 
     const handleChange = (e) => {
@@ -21,7 +23,7 @@ export default function Checker({userToken, setGuest}){
         }
         setCheckForm({ ...checkForm, [e.target.name]: e.target.value });
     };
-    const handleSubmit = async (e) => {
+    const startEvaluationProccess = async (e) => {
         e.preventDefault()
         if(checkForm.text.trim().length > 0){
         try {
@@ -39,27 +41,19 @@ export default function Checker({userToken, setGuest}){
             );
 
             // Retrieve the token from the response's containedObject
-            const res = response.data.containedObject.text;
+            const id = response.data.containedObject.id;
+            const {currentStep, allSteps} = response.data.containedObject;
             // Save the token to local storage
-            setAnswer(res);
-
-            // set and display response
-            // setStatus(res.status);
-            // const responseStatus = await axios.get(
-            //     `http://localhost:8080/app/status/${res.id}`,
-            //     {
-            //         'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json'}
-            // );
-            //
-            // // Retrieve the token from the response's containedObject
-            // const status = responseStatus.data.containedObject;
+            setEvaluationId(id);
+            setProgress({currentStep, allSteps});
+            setIsLoading(true);
 
         } catch (error) {
             console.error("Error logging in:", error);
             await toast.error(
                 error.response
-                    ? error.response.data.message
-                        ? error.response.data.message
+                    ? error.response.data.error
+                        ? error.response.data.error
                         : "Wystąpił błąd przy ewaluacji tekstu."
                     : "Wystąpił błąd przy ewaluacji tekstu",{
                     autoClose: 3000,
@@ -70,6 +64,32 @@ export default function Checker({userToken, setGuest}){
             setTextEmpty(true);
         }
     };
+    useEffect(() => {
+        if (!evaluationId) return;
+
+        const interval = setInterval(async () => {
+            try {
+                const res = await axios.get(`${process.env.REACT_APP_LOGIC_API}/app/status/${evaluationId}`);
+                const { currentStep, allSteps } = res.data.containedObject;
+                setProgress({ currentStep, allSteps });
+
+                if (currentStep === allSteps) {
+                    clearInterval(interval);
+
+                    const resultRes = await axios.get(`${process.env.REACT_APP_LOGIC_API}/app/result/${evaluationId}`);
+                    setResult(resultRes.data.containedObject);
+                    setIsLoading(false);
+                }
+            } catch (err) {
+                console.error(err);
+                toast.error('Error while polling status.', {autoClose: 3000});
+                clearInterval(interval);
+                setIsLoading(false);
+            }
+        }, 3000); // every 3s
+
+        return () => clearInterval(interval);
+    }, [evaluationId]);
 
     setGuest(false);
 
@@ -80,7 +100,7 @@ export default function Checker({userToken, setGuest}){
                     <h2 className="card-title">Sprawdź treść</h2>
                 </div>
                 <div className="card-body">
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={startEvaluationProccess}>
                         <div className="form-row">
                             <div className="form-validation-input">
                                 <textarea
@@ -107,12 +127,40 @@ export default function Checker({userToken, setGuest}){
                         </div>
                         <div className={"form-row"}>
                             <div className={"btn-container"}>
-                                <button className={"btn btn-primary"}>Sprawdź</button>
+                                <button type={"submit"} className={"btn btn-primary"} disabled={isLoading}>Sprawdź</button>
                             </div>
                         </div>
                     </form>
                     <div className="answer-container">
-                        <div className="answer">{answer}</div>
+                        {isLoading ?
+                            <div className="load-bar"><div className={"progress"} style={{width: `${(progress.currentStep/progress.allSteps)*100}%`}}></div></div>
+                            : null}
+                        {result ?
+                            <div style={{display: "contents"}}>
+                        <div className={"answer-row"}><h2 className={"header"}>Wynik</h2> </div>
+                        <div className={"answer-row"}><div className={"label"}>Werdykt:</div> <div className={"value"}> {result.result.label}</div></div>
+                        <div className={"answer-row"}><div className={"label"}>Ocena:</div>
+                            <div className={"value"}>{result.result.finalScore}</div></div>
+                            <div className={"answer-row"}><div className={"label"}>Wyjaśnienie:</div>
+                        </div>
+                            <div className={"answer-row"}><div className={"value"}>{result.result.explanation}</div></div>
+
+                        {/*    {result.result.references?.length > 0 && (*/}
+                        {/*    <div style={{display: "contents"}}>*/}
+                        {/*        <div className={"answer-row"}><h4 className={"header"}>References:</h4></div>*/}
+                        {/*        <ul className="list-disc list-inside">*/}
+                        {/*            {result.result.references.map((ref, index) => (*/}
+                        {/*                <li key={index}>*/}
+                        {/*                    <a href={ref.url} target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">*/}
+                        {/*                        {ref.url}*/}
+                        {/*                    </a>*/}
+                        {/*                </li>*/}
+                        {/*            ))}*/}
+                        {/*        </ul>*/}
+                        {/*    </div>*/}
+                        {/*)}*/}
+                            </div>
+                            : null }
                     </div>
                 </div>
             </div>
